be32a18f85e00f98c804e812caebe41c62fdda1b
Marcio Antonio Moares Reyes <reyesmarcio@yahoo.com.br>
2020-07-16 18:48:01 -0300

    Create voucherGen.php
    Arquivo PHP que gera a Página Dinâmica dos Vereadores baseada nos dados do 
    'form' em index.html

---
+++ voucherGen.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Programa desenvolvido por Marcio A. M. Reyes para
+Imperium Digital Marketing Político
+*/
+
+$bkgImg     =    './arquivos/img'.$_POST['inBkgImg'];
+$eventName  =    $_POST['inEventName'];
+$headline   =    $_POST['inHeadline'];
+$opening    =    $_POST['inOpening'];
+$quote      =    $_POST['inQuote'];
+$attrib     =    $_POST['inAttrib'];
+$font1      =    $_POST['fonts1'];
+$font2      =    $_POST['fonts2'];
+$font3      =    $_POST['fonts3'];
+$font4      =    $_POST['fonts4'];
+$dateFr     =    $_POST['inFrDate'];
+$dateTo     =    $_POST['inToDate'];
+$textOffer1 =    $_POST['inOffer1'];
+$addOffer1  =    $_POST['inAdditional1'];
+$textOffer2 =    $_POST['inOffer2'];
+$addOffer2  =    $_POST['inAdditional2'];
+$qrSize     =    $_POST['inQrSize']; 
+$qrData1    =    $_POST['inQrData1'];
+$qrData2    =    $_POST['inQrData2'];
+
+?>
